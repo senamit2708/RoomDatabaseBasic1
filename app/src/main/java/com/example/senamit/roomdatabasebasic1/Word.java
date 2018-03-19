59e35486b5d37ea9473001eae5d5f7ae3b30f4d9
senamit2708 <senamit2708@gmail.com>
2018-03-20 00:53:58 +0530

    @ENTITY CLASS is added, for defining the table with its column name in it.

---
+++ app/src/main/java/com/example/senamit/roomdatabasebasic1/Word.java
@@ -0,0 +1,30 @@
+package com.example.senamit.roomdatabasebasic1;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by senamit on 20/3/18.
+ */
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    @NonNull
+    public String getWord() {
+        return mWord;
+    }
+}
