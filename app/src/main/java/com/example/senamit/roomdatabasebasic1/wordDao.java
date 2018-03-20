9ac76143f03f2b59fceaf5428620e911b16aba1a
senamit2708 <senamit2708@gmail.com>
2018-03-20 21:38:54 +0530

    DAO class is created

---
+++ app/src/main/java/com/example/senamit/roomdatabasebasic1/wordDao.java
@@ -0,0 +1,26 @@
+package com.example.senamit.roomdatabasebasic1;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by senamit on 20/3/18.
+ */
+@Dao
+public interface wordDao {
+
+    @Insert
+     void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+     void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+}
